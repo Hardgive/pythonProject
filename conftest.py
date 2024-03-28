eac410f9297f45d3293fbb329582e8e898be857e
Петров Юрий Николаевич <hardgive@yandex.ru>
2024-03-28 17:13:48 +0300

    Started to add new headless parameter

--- conftest.py
+++ conftest.py
@@ -7,13 +7,23 @@ def url(request):
     return request.config.getoption("--url")
 
 
+def headless(request):
+    return request.config.getoption("--headless")
+
+
 @pytest.fixture(params=["chrome"], scope="class")
 def driver_init(request):
     if request.param == "chrome":
         options = Options()
-        options.add_argument('--headless')
+        if headless == 'true':
+            options.add_argument('--headless')
+        elif headless == 'false':
+            options.add_argument('')
+        else:
+            print(f'Неверное значение параметра --headless={headless}. Передано как значение по умолчанию --headless=true')
+            options.add_argument('--headless')
         options.add_argument('--disable-gpu')
-        driver = webdriver.Chrome(chrome_options=options)
+        driver = webdriver.Chrome(options=options)
     request.cls.driver = driver
     yield
     driver.close()
@@ -25,8 +35,13 @@ def url(request):
     return request.config.getoption("--url")
 
 
+def headless(request):
+    return request.config.getoption("--headless")
+
+
 def pytest_addoption(parser):
     parser.addoption("--url", action="store", default="https://www.saucedemo.com/")
+    parser.addoption("--headless", action="store", default="12315")
 
 
 def test_variables():
